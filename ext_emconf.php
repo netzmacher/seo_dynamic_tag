f8a18cc0e1badd087b3632b8562a12538dc4287e
Dirk Wildt <wildt@die-netzmacher.de>
2016-05-10 21:12:05 +0200

    Initial Git Release

---
+++ ext_emconf.php
@@ -0,0 +1,36 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'SEO Dynamic Tag 3',
+	'description' => 'Search Engine Optimization (SEO) for the title tag, the canonical tag and the meta tags author, description and keywords.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '3.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Dirk Wildt (Die Netzmacher)',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+  'constraints' => array(
+    'depends' => array(
+      'typo3' => '4.5.0-7.4.99',
+    ),
+    'conflicts' => array(
+    ),
+    'suggests' => array(
+    ),
+  ),
+);
+
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+  die ('Access denied.');
+}
+
+t3lib_extMgm::addPItoST43($_EXTKEY,'2.x/pi1/class.tx_seodynamictag_pi1.php','_pi1','',1);
