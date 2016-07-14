04ee23b811bde00a671f7a3ca0e6439913157793
Dirk Wildt <wildt@die-netzmacher.de>
2016-07-14 11:13:57 +0200

    3.1.2

--- ext_tables.php
+++ ext_tables.php
@@ -5,11 +5,11 @@ if( ! defined ( 'TYPO3_MODE' ) )
 }
 
   //  add static TypoScript
-t3lib_extMgm::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/Base/',          'SEO [1]'                       );
-t3lib_extMgm::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/ResetPageMeta/', 'SEO [9] Reset page.meta'       );
-t3lib_extMgm::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/',                             'SEO [90] 2.x'                  );
-t3lib_extMgm::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/cal/',                         'SEO [91] + 2.x cal'            );
-t3lib_extMgm::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/pages/',                       'SEO [91] + 2.x pages'          );
-t3lib_extMgm::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/tt_news/',                     'SEO [91] + 2.x tt_news'        );
-t3lib_extMgm::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/tt_products/',                 'SEO [91] + 2.x tt_products'    );
-t3lib_extMgm::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/resetPageMeta/',               'SEO [99] 2.x reset page.meta'  );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/Base/',          'SEO [1]'                       );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Configuration/TypoScript/ResetPageMeta/', 'SEO [9] Reset page.meta'       );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/',                             'SEO [90] 2.x'                  );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/cal/',                         'SEO [91] + 2.x cal'            );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/pages/',                       'SEO [91] + 2.x pages'          );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/tt_news/',                     'SEO [91] + 2.x tt_news'        );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/tt_products/',                 'SEO [91] + 2.x tt_products'    );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( $_EXTKEY, 'Resources/Private/2.x/static/resetPageMeta/',               'SEO [99] 2.x reset page.meta'  );
