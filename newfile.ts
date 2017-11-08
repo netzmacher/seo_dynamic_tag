aa9a3de72d08d5adb712903cd8fdd78b20aaf675
Dirk Wildt <wildt@die-netzmacher.de>
2017-11-08 17:40:54 +0100

    3.3.0

--- newfile.ts
+++ newfile.ts
@@ -3,7 +3,7 @@
 		canonical.single.additionalParams = &tx_typo3forum_pi1[forum]={GP:tx_typo3forum_pi1|forum}
 	  condition.single.begin = globalVar = GP:tx_typo3forum_pi1|forum > 0] && [globalVar = TSFE:id = 1120
 		database.field.description = title
-		database.field.title.description = description
+		database.field.description.appendix = description
 		database.field.keywords = title
 		database.field.title = title
 		database.field.title.appendix = description
@@ -11,9 +11,8 @@
 	  database.table = tx_typo3forum_domain_model_forum_forum
 	  database.gp = tx_typo3forum_pi1|forum
 		default.author = Netzmacher-Forum Responsive TYPO3
-	  description.prefix = Forum
+	  description.prefix = Responsive Forum |
 		keywords.appendix = TYPO3,responsive
-		title.prefix = Responsive Forum
-		title.prefix = Responsive Forum
+		title.prefix = Responsive Forum | 
 	}
 [END]
