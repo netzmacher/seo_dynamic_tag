a4f6208c90cb76ca8e12186f1e8e6b6b4581bf7d
Dirk Wildt <wildt@die-netzmacher.de>
2018-11-25 20:53:50 +0100

    0.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Search Engine Optimisation (SEO) for title tag, canonical tag, meta tags author, description and keywords and social media tags for google, opengraph/facebook and twitter.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '4.0.1',
+	'version' => '4.0.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
   'constraints' => array(
     'depends' => array(
-      'typo3' => '4.5.0-8.9.99',
+      'typo3' => '4.5.0-9.9.99',
     ),
     'conflicts' => array(
     ),
