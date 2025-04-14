3875738fc20d277d6f25e4c3ac8b23e77a26e5bd
Dirk Wildt <dirk.wildt@verdigado.com>
2025-04-14 13:46:51 +0200

    [TASK] Update TypoScript: *.txt, *.ts > *.typoscript #6774

--- Configuration/TypoScript/Base/_constants/_constants.ts
+++
@@ -1,5 +0,0 @@
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:seo_dynamic_tag/Configuration/TypoScript/Base/_constants/canonical.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:seo_dynamic_tag/Configuration/TypoScript/Base/_constants/date.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:seo_dynamic_tag/Configuration/TypoScript/Base/_constants/image.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:seo_dynamic_tag/Configuration/TypoScript/Base/_constants/meta.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:seo_dynamic_tag/Configuration/TypoScript/Base/_constants/socialmedia.ts">
